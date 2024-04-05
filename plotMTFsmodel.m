83b616b7a3d3ae9550152cdb43c4b1f47786e8f0
Audrey Drotos <12617059+audreydrotos@users.noreply.github.com>
2024-04-05 12:57:32 -0400

    add g_syn1 to function call

--- plotMTFsmodel.m
+++ plotMTFsmodel.m
@@ -1,7 +1,8 @@
 function plotMTFsmodel()
 
-% what NMDA conductances do we want to use?
-g_syn2_list = [0.10];
+% what AMPA & NMDA conductances do we want to use?
+g_syn1 = 0.4;
+g_syn2_list = [0 0.10];
 
 % set params as the same we ran for the fMod
 fModStart = 16;
@@ -28,7 +29,7 @@ phase_NMDA = [];
 for j = 1:length(g_syn2_list)
     for i = 1:numFreqs
         % calculate the number of spikes at that fMod
-        spiketimes = NMDAmodel(fMod(i), g_syn2_list(j));
+        spiketimes = NMDAmodel(fMod(i), g_syn1, g_syn2_list(j));
         count = length(spiketimes);
     
         % pull out the fMod 
