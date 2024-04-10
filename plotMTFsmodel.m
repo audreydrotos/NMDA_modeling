f8b9636d4d399fb71e84238cabc4b5ffb5068044
Audrey Drotos <12617059+audreydrotos@users.noreply.github.com>
2024-04-10 13:31:20 -0400

    update colors

--- plotMTFsmodel.m
+++ plotMTFsmodel.m
@@ -1,8 +1,9 @@
 function plotMTFsmodel()
 
 % what AMPA & NMDA conductances do we want to use?
-g_syn1 = 0.4;
-g_syn2_list = [0 0.10];
+g_syn1 = 0.2;
+g_syn2_list = [0 0.05];
+g_l = 0.7 % VIP neuron
 
 % set params as the same we ran for the fMod
 fModStart = 16;
@@ -29,7 +30,7 @@ phase_NMDA = [];
 for j = 1:length(g_syn2_list)
     for i = 1:numFreqs
         % calculate the number of spikes at that fMod
-        spiketimes = NMDAmodel(fMod(i), g_syn1, g_syn2_list(j));
+        spiketimes = NMDAmodel(fMod(i), g_syn1, g_syn2_list(j), g_l);
         count = length(spiketimes);
     
         % pull out the fMod 
@@ -60,9 +61,9 @@ end
 %% plot the rate MTF
 figure('Position', [0 0 1000 500])
 subplot(1, 3, 1)
-plot(log2(fMod), spikeCounts_AMPA(:), 'Color', '#e76f51');
+plot(log2(fMod), spikeCounts_AMPA(:), 'Color', '#2a9d8f');
 hold on
-plot(log2(fMod), spikeCounts_NMDA(:), 'Color', '#2a9d8f');
+plot(log2(fMod), spikeCounts_NMDA(:), 'Color', '#e76f51');
 
 % label x axis ticks to show 3 tick labels
 xticks([log2(fMod(1)) log2(fMod(ceil(length(fMod)/2))) log2(fMod(end))]);
@@ -77,9 +78,9 @@ legend('AMPA only', 'AMPA + NMDA'); % Add legend
 
 %% plot the temporal MTF
 subplot(1, 3, 2)
-plot(log2(fMod), synchrony_AMPA(:), 'Color', '#e76f51');
+plot(log2(fMod), synchrony_AMPA(:), 'Color', '#2a9d8f');
 hold on
-plot(log2(fMod), synchrony_NMDA(:), 'Color', '#2a9d8f');
+plot(log2(fMod), synchrony_NMDA(:), 'Color', '#e76f51');
  
 % set y lim
 ylim([0 1]);
@@ -113,11 +114,11 @@ hold(pax, 'on');
 logFreqs = log2(fMod);
 
 for i = 1:length(phase_AMPA)
-    polarplot(pax, phase_AMPA(i), logFreqs(i), 'o', 'MarkerEdgeColor', 'none', 'MarkerFaceColor', '#e76f51');
+    polarplot(pax, phase_AMPA(i), logFreqs(i), 'o', 'MarkerEdgeColor', 'none', 'MarkerFaceColor', '#2a9d8f');
 end
 
 for i = 1:length(phase_NMDA)
-    polarplot(pax, phase_NMDA(i), logFreqs(i), 'o', 'MarkerEdgeColor', 'none', 'MarkerFaceColor', '#2a9d8f');
+    polarplot(pax, phase_NMDA(i), logFreqs(i), 'o', 'MarkerEdgeColor', 'none', 'MarkerFaceColor', '#e76f51');
 end
 
 hold(pax, 'off');
