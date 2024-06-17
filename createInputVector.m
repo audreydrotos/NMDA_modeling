2d3941ae9306588d5de54a683b274422e122a4ed
Audrey Drotos <12617059+audreydrotos@users.noreply.github.com>
2024-06-17 15:12:13 -0400

    create input vector

---
+++ createInputVector.m
@@ -0,0 +1,17 @@
+function inputs = createInputVector(modFreq)
+
+% Time duration of the simulation
+t_final=1000; % duration of simulation in msec
+
+% Create vector with inputs evenly spaced
+inputs = linspace(0, t_final, modFreq);
+
+% Add +/- 3ms jitter to each input
+% First, generate vector of n = modFreq numbers from a gaussian
+% distribution
+jitter = randn(1, modFreq);
+
+% Add these together
+inputs = inputs + jitter;
+
+end % for function
