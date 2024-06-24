074d32a6495aabf848d307e33c896ab307b3f454
Audrey Drotos <12617059+audreydrotos@users.noreply.github.com>
2024-06-23 21:44:14 -0400

    add multiple model iterations

--- plotMTFsLeaksmodel.m
+++ plotMTFsLeaksmodel.m
@@ -1,5 +1,5 @@
 function plotMTFsLeaksmodel()
-%% what g_L conductances do we wnat to use?
+%% what g_L conductances do we want to use?
 % Generate the vector (pseudo log spaced now)
 logSpacedVector = [0.2820, 0.5, 0.7499, 1.9940, 5.3025];
 
@@ -31,67 +31,78 @@ synchrony_NMDA = [];
 phase_NMDA = [];
 
 for j = 1:length(g_l_list)
+    disp(['Running leak conductance: ' num2str(j)]);
     for i = 1:numFreqs
-        % calculate the number of spikes at that fMod for AMPA + NMDA
-        spiketimes = NMDAmodel(fMod(i), g_syn1_list(j), g_syn2_list(j), g_l_list(j));
-        count = length(spiketimes);
-    
-        % pull out the fMod 
-        fm = fMod(i);
+        for q = 1:5 % iterate over 5 times
+            % calculate the number of spikes at that fMod for AMPA + NMDA
+            spiketimes = NMDAmodel(fMod(i), g_syn1_list(j), g_syn2_list(j), g_l_list(j));
+            count = length(spiketimes);
         
-        if count > 4
-            % convert spike times to radians to calculate vector strength
-            spikeRads = mod(spiketimes/1000, 1/fm) * 2 * pi * fm; % spike times need to be in s for this and code gives in ms
+            % pull out the fMod 
+            fm = fMod(i);
             
-            % second, calculate vector strength. p2 of GOldberg and Brown 1969
-            % method
-            x = cos(spikeRads);
-            y = sin(spikeRads);
-            r = sqrt(sum(x)^2 + sum(y)^2)/length(x);
-        else
-            spikeRads = NaN;
-            r = NaN;
-        end 
-
-        % add to the appropriate vector
-        spikeCounts_NMDA(i) = count;
-        synchrony_NMDA(i) = r;
-        phase_NMDA(i) = mean(spikeRads);
-
-        % calculate the number of spikes at that fMod for AMPA only
-        spiketimes = NMDAmodel(fMod(i), g_syn1_list(j), 0, g_l_list(j));
-        count = length(spiketimes);
+            if count > 4
+                % convert spike times to radians to calculate vector strength
+                spikeRads = mod(spiketimes/1000, 1/fm) * 2 * pi * fm; % spike times need to be in s for this and code gives in ms
+                
+                % second, calculate vector strength. p2 of GOldberg and Brown 1969
+                % method
+                x = cos(spikeRads);
+                y = sin(spikeRads);
+                r = sqrt(sum(x)^2 + sum(y)^2)/length(x);
+            else
+                spikeRads = NaN;
+                r = NaN;
+            end 
     
-        % pull out the fMod 
-        fm = fMod(i);
-
-        if count > 4
-            % convert spike times to radians to calculate vector strength
-            spikeRads = mod(spiketimes/1000, 1/fm) * 2 * pi * fm; % spike times need to be in s for this and code gives in ms
-            
-            % second, calculate vector strength. p2 of GOldberg and Brown 1969
-            % method
-            x = cos(spikeRads);
-            y = sin(spikeRads);
-            r = sqrt(sum(x)^2 + sum(y)^2)/length(x);
-        else
-            spikeRads = NaN;
-            r = NaN;
+            % add to the appropriate vector
+            spikeCounts_NMDA(q,i) = count;
+            synchrony_NMDA(q,i) = r;
+            phase_NMDA(q,i) = mean(spikeRads);
+    
+            % calculate the number of spikes at that fMod for AMPA only
+            spiketimes = NMDAmodel(fMod(i), g_syn1_list(j), 0, g_l_list(j));
+            count = length(spiketimes);
+        
+            % pull out the fMod 
+            fm = fMod(i);
+    
+            if count > 4
+                % convert spike times to radians to calculate vector strength
+                spikeRads = mod(spiketimes/1000, 1/fm) * 2 * pi * fm; % spike times need to be in s for this and code gives in ms
+                
+                % second, calculate vector strength. p2 of GOldberg and Brown 1969
+                % method
+                x = cos(spikeRads);
+                y = sin(spikeRads);
+                r = sqrt(sum(x)^2 + sum(y)^2)/length(x);
+            else
+                spikeRads = NaN;
+                r = NaN;
+            end
+    
+            % add to the appropriate vector
+            spikeCounts_AMPA(q,i) = count;
+            synchrony_AMPA(q,i) = r;
+            phase_AMPA(q,i) = nanmean(spikeRads);
         end
 
-        % add to the appropriate vector
-        spikeCounts_AMPA(i) = count;
-        synchrony_AMPA(i) = r;
-        phase_AMPA(i) = nanmean(spikeRads);
-
     end
-    
+
+    % add to vectors to average across each fMod for each iteration
+    XspikeCounts_AMPA = mean(spikeCounts_AMPA);
+    Xsynchrony_AMPA = mean(synchrony_AMPA);
+    Xphase_AMPA = mean(phase_AMPA);
+    XspikeCounts_NMDA = mean(spikeCounts_NMDA);
+    Xsynchrony_NMDA = mean(synchrony_NMDA);
+    Xphase_NMDA = mean(phase_NMDA);
+
     %% plot the rate MTF
     figure('Position', [0 0 700 150])
     subplot(1, 3, 1)
-    plot(log2(fMod), spikeCounts_AMPA(:), 'Color', '#2a9d8f');
+    plot(log2(fMod), XspikeCounts_AMPA(:), 'Color', '#2a9d8f');
     hold on
-    plot(log2(fMod), spikeCounts_NMDA(:), 'Color', '#e76f51');
+    plot(log2(fMod), XspikeCounts_NMDA(:), 'Color', '#e76f51');
     
     % label x axis ticks to show 3 tick labels
     xticks([log2(fMod(1)) log2(fMod(ceil(length(fMod)/2))) log2(fMod(end))]);
@@ -108,9 +119,9 @@ for j = 1:length(g_l_list)
     
     %% plot the temporal MTF
     subplot(1, 3, 2)
-    plot(log2(fMod), synchrony_AMPA(:), 'Color', '#2a9d8f');
+    plot(log2(fMod), Xsynchrony_AMPA(:), 'Color', '#2a9d8f');
     hold on
-    plot(log2(fMod), synchrony_NMDA(:), 'Color', '#e76f51');
+    plot(log2(fMod), Xsynchrony_NMDA(:), 'Color', '#e76f51');
      
     % set y lim
     ylim([0 1]);
@@ -145,11 +156,11 @@ for j = 1:length(g_l_list)
     logFreqs = log2(fMod);
     
     for i = 1:length(phase_AMPA)
-        polarplot(pax, phase_AMPA(i), logFreqs(i), 'o', 'MarkerEdgeColor', 'none', 'MarkerFaceColor', '#2a9d8f');
+        polarplot(pax, Xphase_AMPA(i), logFreqs(i), 'o', 'MarkerEdgeColor', 'none', 'MarkerFaceColor', '#2a9d8f');
     end
     
     for i = 1:length(phase_NMDA)
-        polarplot(pax, phase_NMDA(i), logFreqs(i), 'o', 'MarkerEdgeColor', 'none', 'MarkerFaceColor', '#e76f51');
+        polarplot(pax, Xphase_NMDA(i), logFreqs(i), 'o', 'MarkerEdgeColor', 'none', 'MarkerFaceColor', '#e76f51');
     end
     
     hold(pax, 'off');
