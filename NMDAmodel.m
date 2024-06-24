cfc47e398a0379815a9fc97fb269b52ce55086fd
Audrey Drotos <12617059+audreydrotos@users.noreply.github.com>
2024-06-23 21:44:48 -0400

    comment out plots

--- NMDAmodel.m
+++ NMDAmodel.m
@@ -50,38 +50,28 @@ else
 end
 
 % plot output
-subplot(4,1,1);
-plot(t,v,'Color', '#03045e','Linewidth',2);
-set(gca,'Fontsize',16);
-xlabel('t [ms]','Fontsize',20); ylabel('v [mV]','Fontsize',20);
-ylim([-65 20])
-
-subplot(4,1,2)
-plot(t,s1*g_syn1,'Color','#0077b6','Linewidth',2)
-set(gca,'Fontsize',16);
-xlabel('t [ms]','Fontsize',20); ylabel('ampa current gate','Fontsize',16);
-ylim([0 0.2])
-
-subplot(4,1,3)
-plot(t,s2*g_syn2,'Color','#00b4d8','Linewidth',2)
-set(gca,'Fontsize',16);
-xlabel('t [ms]','Fontsize',20); ylabel('nmda current gate','Fontsize',16);
-ylim([0 0.2])
-
+% subplot(4,1,1);
+% plot(t,v,'Color', '#03045e','Linewidth',2);
+% set(gca,'Fontsize',16);
+% xlabel('t [ms]','Fontsize',20); ylabel('v [mV]','Fontsize',20);
+% ylim([-65 20])
+% 
 % subplot(4,1,2)
-% plot(t,s1,'Color','#0077b6','Linewidth',2)
+% plot(t,s1*g_syn1,'Color','#0077b6','Linewidth',2)
 % set(gca,'Fontsize',16);
 % xlabel('t [ms]','Fontsize',20); ylabel('ampa current gate','Fontsize',16);
+% ylim([0 0.2])
 % 
 % subplot(4,1,3)
-% plot(t,s2,'Color','#00b4d8','Linewidth',2)
+% plot(t,s2*g_syn2,'Color','#00b4d8','Linewidth',2)
 % set(gca,'Fontsize',16);
 % xlabel('t [ms]','Fontsize',20); ylabel('nmda current gate','Fontsize',16);
-
-subplot(4,1,4)
-plot(t,s1+s2,'Color','#90e0ef','Linewidth',2)
-set(gca,'Fontsize',16);
-xlabel('t [ms]','Fontsize',20); ylabel('summed current gate','Fontsize',16);
+% ylim([0 0.2])
+% 
+% subplot(4,1,4)
+% plot(t,s1+s2,'Color','#90e0ef','Linewidth',2)
+% set(gca,'Fontsize',16);
+% xlabel('t [ms]','Fontsize',20); ylabel('summed current gate','Fontsize',16);
 
 function dvarsdt = modeleqs(t,vars,inputs)
 % WB model point neuron equations
